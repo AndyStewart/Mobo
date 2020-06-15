68a239a8f4abe6ecf5191e019c995eb56e7dba20
Andrew Stewart <andrew.stewart@i-nnovate.net>
2020-06-15 19:15:01 +0100

    Reset to blank

--- Mobo.AcceptanceTests/TimerTests.cs
+++ Mobo.AcceptanceTests/TimerTests.cs
@@ -6,29 +6,6 @@ namespace Mobo.AcceptanceTests
 {
     public class TimerTests
     {
-        [Fact]
-        public void StartingTheTimerSetsTheTurnToTheFirstPerson()
-        {
-            using var mobo = new Mobo();
-            mobo.Create("Mob Name");
-            mobo.AddMembers("Andy");
-            mobo.StartTimer();
-            mobo.MemberTurnIs("Andy");
-        }
-        
-        [Theory]
-        [InlineData(3)]
-        [InlineData(2)]
-        [InlineData(1)]
-        public async Task TimerCountsDownEachSecond(int delay)
-        {
-            using var mobo = new Mobo();
-            mobo.Create("Mob Name");
-            mobo.AddMembers("Andy");
-            mobo.StartTimer();
-            await Task.Delay(TimeSpan.FromSeconds(delay));
-            mobo.TimeTillNextTurnIs(new TimeSpan(0, 14, 60 - delay));
-        }
 
     }
 }
